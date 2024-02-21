54e4abb9d4e08c628bdd92ac7dac5ef961a56f33
Deepak Mehar <deepakmehar397@gmail.com>
2024-02-21 15:01:39 +0530

    nav

--- nav.php
+++ nav.php
@@ -1,29 +1,47 @@
-<nav>
-    <div class="left">logo</div>
-    <div class="middle">
-        <a href="/car_rent/home.php" class="navItem">HOME</a>
-        <a href="/car_rent/allcars.php" class="navItem">CARS</a>
-        <a href="/car_rent/about.php" class="navItem">ABOUT</a>
-        <a href="/car_rent/contact.php" class="navItem">CONTACT</a>
-    </div>
-    <div class="right">profile,history,logout</div>
-</nav>
-<style>
-    nav{
-        height: 40px;
-        /* width: 100%; */
-        background-color: red;
-        border-radius:16px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 5px;
-    }
-      nav  .middle a{text-decoration: none;}
-      nav  .middle{
-            display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap:5px;
-        }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="left">DEEPAK</div>
+      <ul id="middle">
+        <li>
+          <a class="navitem" href="/car_rent/"> Home</a>
+        </li>
+        <li>
+          <a class="navitem" href="/car_rent/allcars.php"> Cars</a>
+        </li>
+        <li>
+          <a class="navitem" href="/car_rent/about.php"> About</a>
+        </li>
+        <li>
+          <a class="navitem" href="/car_rent/contact.php"> Contact</a>
+        </li>
+      </ul>
+      <div class="right"><?php
+        if (isset($_COOKIE['user_data'])) {
+            echo '<a href="/car_rent/user.php" class="navItem">profile</a>';
+            echo '<a href="/car_rent/rantals.php" class="navItem">HISTORY</a>';
+            echo '<a href="/car_rent/logout.php" class="navItem">LOGOUT</a>';
+
+        } else {
+            echo '<a href="/car_rent/login.php" class="navItem">LOGIN</a>';
+            echo '<a href="/car_rent/register.php" class="navItem">REGISTER</a>';
+        }?></div>
+    </nav>
+    <script>
+      function stick_navigatio() {
+        window.addEventListener("scroll", function () {
+          var nav = this.document.querySelector("nav");
+          nav.classList.toggle("sticky", window.scrollY > 0);
+        });
+      }
+      stick_navigatio();
+    </script>
+  </body>
+</html>
